637aca59b997d7fdee2db4f3e382e6a121fd2a14
rmhg <39510036+rmhg@users.noreply.github.com>
2020-02-26 18:11:54 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -1,10 +1,15 @@
 var lat = document.getElementById("lat");
 var long = document.getElementById("long");
 var speed = document.getElementById("speed").childNodes[3];
+var hs = document.getElementById("speed").childNodes[1];
 var overlay  = document.getElementById("overlay");
 var opt = document.getElementById("option");
+var opt0 = document.getElementById("option0");
 var acc = document.getElementById("speed").childNodes[5];
-var head = document.getElementById("head");
+var head = document.getElementById("speed").childNodes[7];
+var ssw = document.getElementById("menu").childNodes[3];
+var ms = true;
+var fact = 1;
 var oldts = 0;
 var l = 0;
 var ll = 0;
@@ -24,9 +29,20 @@ function l2m(lat1,lat2,lon1,lon2)
     var m = R*c;
     return m;
 }
+ssw.onclick = ()=>{
+    ms = !ms;
+    if(ms)
+        hs.innerHTML = ssw.innerHTML = "SPEED (m/s)";
+    else
+        hs.innerHTML = ssw.innerHTML = "SPEED (KM/h)";
+}
 opt.addEventListener("click",()=>{
-    opt.style.display = "none";
+   opt.style.display = "none";
     document.getElementById("menu").style.display = "flex";
+});
+opt0.addEventListener("click",()=>{
+   opt.style.display = "flex";
+    document.getElementById("menu").style.display = "none";
 });
 function sspeed(d,t)
 {
@@ -40,13 +56,17 @@ this.navigator.geolocation.watchPosition((pos)=>{
     lat.innerHTML = "Latitude : " + pos.coords.latitude;
     long.innerHTML = "Longitude : " + pos.coords.longitude;
     var se = sspeed(l2m(l,ll,nl,nll),(pos.timestamp - oldts)/1000);
-    speed.innerHTML = "Speed : " + pos.coords.speed;
+    console.log(se);
+    if(!ms)
+        fact = 3.6;
+    else
+        fact = 1;
+    speed.innerHTML = "Speed : " + pos.coords.speed*fact;
     acc.innerHTML = "Accuracy : " + pos.coords.accuracy;
     head.innerHTML = "Heading : " + pos.coords.heading;
     oldts = pos.timestamp;
     l = nl;
     ll = nll;
 },()=>{
-    console.log("ERROR");
     overlay.style.display = "flex";
-},{enableHighAccuracy:true,timeout:5000,maximumAge:1000000});
+},{enableHighAccuracy:true,timeout:50000,maximumAge:1000000});
